a08e879ebe8e251c38dd559a8cd4db6924cae482
E Alexander Wheeldon <eawheeldon@gmail.com>
2017-03-17 13:19:56 -0400

    converting results to a dataframe with the right column order

--- dndMonstersScraper.py
+++ dndMonstersScraper.py
@@ -24,8 +24,15 @@ def parse_monster_data(monsters):
 		monster_data.append(data)
 	return monster_data
 
+def make_monsters_dataFrame(monsters):
+	data = pd.DataFrame(monsters, columns = ['name', 'type', 'size', 'alignment', 'challenge'])
+	return data
+
 if __name__ == "__main__":
 	html = urllib.urlopen("http://www.orcpub.com/dungeons-and-dragons/5th-edition/monsters")
 	monster_manual = get_monster_list(bs4.BeautifulSoup(html, 'html.parser'))
-	print(parse_monster_data(monster_manual[0:5]))
+	monster_manual = parse_monster_data(monster_manual[0:5])
+	monster_manual = make_monsters_dataFrame(monster_manual)
+
+
 
