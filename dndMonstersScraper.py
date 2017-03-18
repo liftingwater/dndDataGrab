c92f1dd07aca51988d44ba6dade3140d8cb4d84a
E Alexander Wheeldon <eawheeldon@gmail.com>
2017-03-18 13:23:06 -0400

    trying to get monster details, having trouble parsing through html

--- dndMonstersScraper.py
+++ dndMonstersScraper.py
@@ -28,11 +28,30 @@ def make_monsters_dataFrame(monsters):
 	data = pd.DataFrame(monsters, columns = ['name', 'type', 'size', 'alignment', 'challenge'])
 	return data
 
+def get_all_monster_details(monsters): 
+	for monster in monsters: 
+		html = urllib.urlopen("http://www.orcpub.com/dungeons-and-dragons/5th-edition/monsters/" + monster.lower())
+		monster_details_on_page = parse_monster_details(bs4.BeautifulSoup(html, 'html.parser'))
+		monster_details_on_page["name"] = monster
+		return monster_details_on_page
+
+def parse_monster_details(html_soup): 
+	final_monster_details = {}
+	monster_data = html_soup.find_all("div", {"class": "col-xs-12 col-md-6"})
+	print(len(monster_data))
+	return final_monster_details
+
+
 if __name__ == "__main__":
 	html = urllib.urlopen("http://www.orcpub.com/dungeons-and-dragons/5th-edition/monsters")
 	monster_manual = get_monster_list(bs4.BeautifulSoup(html, 'html.parser'))
-	monster_manual = parse_monster_data(monster_manual[0:5])
-	monster_manual = make_monsters_dataFrame(monster_manual)
+	monster_manual = parse_monster_data(monster_manual[0:1])
+	monster_directory = make_monsters_dataFrame(monster_manual)
+	
+	#print(monster_directory)
+	all_monster_details = get_all_monster_details(monster_directory["name"])
+	print(all_monster_details)
+
 
 
 
