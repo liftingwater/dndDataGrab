9223b9b4c274f825e9697478d4cc00c5e76bc72d
E Alexander Wheeldon <eawheeldon@gmail.com>
2017-03-22 13:55:10 -0400

    Adding code to get ability scores

--- dndMonstersScraper.py
+++ dndMonstersScraper.py
@@ -45,31 +45,31 @@ def parse_monster_details(html_soup):
 
 	for i in [0, 1, 3]: 
 		saved_monster_data.append(re.sub('<[^>]+>', '', str(monster_data[0].find_all("em")[i])))
-
 	monster_stats = monster_data[0].find_all("div", {"class": "char-details-field"})
 
-	#monster_stats_list = []
 	for i in monster_stats: 
 		saved_monster_data.append(re.sub('<[^>]+>', '', str(i.find_all("span")[0])))
-
 	for i in range(len(data_keys)):
 		final_monster_details[data_keys[i]] = saved_monster_data[i]
 
-	#print(monster_stats_list)
+	monster_abilities = ["str", "dex", "con", "int", "wis", "cha"]
+	abilities = monster_data[0].find_all("table")[0].find_all("div")
+	for i in range(len(abilities)): 
+		final_monster_details[monster_abilities[i]] = re.sub('<[^>]+>', '', str(abilities[i]))
 
 	return final_monster_details
 
 def view_monster_details(html_soup): 
 	monster_data = html_soup.find_all("div", {"class": "col-xs-12 col-md-6"})
 	print(monster_data)
-
+	abilities = []
 	print("\n +++++++++++++++++++++++++++++++++++++++++++++ \n")
 
-	monster_stats = monster_data[0].find_all("div", {"class": "char-details-field"})
+	monster_stats = monster_data[0].find_all("table")[0].find_all("div")
 	for i in monster_stats: 
-		print "\n ++++++++++++++ \n"
-		print(i)
-	return None
+		abilities.append(re.sub('<[^>]+>', '', str(i)))
+	print(abilities)
+	return {}
 
 
 if __name__ == "__main__":
