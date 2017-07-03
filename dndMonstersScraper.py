b6e58f312750eb43aedc0b439ee49c03886e8e0f
E Alexander Wheeldon <eawheeldon@gmail.com>
2017-07-03 13:23:15 -0400

    Cleaning up script and clarifying certain comments.

--- dndMonstersScraper.py
+++ dndMonstersScraper.py
@@ -33,6 +33,7 @@ def make_monsters_dataFrame(monsters):
 	data = pd.DataFrame(monsters, columns = ['name', 'type', 'size', 'alignment', 'challenge'])
 	return data
 
+#Convert specific details data to data frame
 def make_monster_details_dataFrame(monsters): 
 	data = pd.DataFrame(monsters, columns = ["name", "size", "type", "alignment", "armor class", "hit points", "speed", "str", "dex", "con", "int", "wis", "cha", "proficiency bonus", "saving throws", "skills", "damage vulnerabilities", "damage resistances", "damage immunities", "condition immunities", "senses", "languages", "challenge"])
 	return data
@@ -41,18 +42,16 @@ def make_monster_details_dataFrame(monsters):
 def get_all_monster_details(monsters): 
 	total_monster_details_list = []
 	for monster in monsters: 
-		#print(re.sub(' ', '-', monster.lower()))
-		#print monster
-		#Visit monster page
+		#regular expressions create url extention
 		monster = re.sub(',|\(|\)|\-|\/|\'', '', monster)
 		print(re.sub(' ', '-', monster.lower()))
+		#retreive data from monster page
 		html = urllib.urlopen("http://www.orcpub.com/dungeons-and-dragons/5th-edition/monsters/" + re.sub(' ', '-', monster.lower()))
-		#print("Now retrieving data from ")
-		#print("http://www.orcpub.com/dungeons-and-dragons/5th-edition/monsters/" + re.sub(' ', '-', monster.lower()))
 		#monster_details_on_page = view_monster_details(bs4.BeautifulSoup(html, 'html.parser'))
 		#This line of code calls the function to get the monster details for that specific monster
 		monster_details_on_page = parse_monster_details(bs4.BeautifulSoup(html, 'html.parser'))
 		monster_details_on_page["name"] = monster
+		#Append data to the master dictionary
 		total_monster_details_list.append(monster_details_on_page)
 	return total_monster_details_list
 
@@ -70,9 +69,7 @@ def parse_monster_details(html_soup):
 	monster_stats = monster_data_html[0].find_all("div", {"class": "char-details-field"})
 	for i in monster_stats:
 		property_names = re.sub('<[^>]+>', '', str(i.find_all("h5")[0]))
-		#print(property_names)
 		property_values = re.sub('<[^>]+>', '', str(i.find_all("span")[0]))
-		#print(property_values)
 		final_monster_details[property_names.lower()] = property_values
 	        
 	#Code for collecting monster abilities scores
@@ -83,6 +80,7 @@ def parse_monster_details(html_soup):
 	#print(final_monster_details)
 	return final_monster_details
 
+#code intended for development
 def view_monster_details(html_soup): 
 	print "Test mode"
 	monster_data_html = html_soup.find_all("div", {"class": "col-xs-12 col-md-6"})
@@ -114,19 +112,11 @@ if __name__ == "__main__":
 	html = urllib.urlopen("http://www.orcpub.com/dungeons-and-dragons/5th-edition/monsters")
 	monster_manual = get_monster_list(bs4.BeautifulSoup(html, 'html.parser'))
 	#Get a list of monsters on the main page: http://www.orcpub.com/dungeons-and-dragons/5th-edition/monsters
-	#print monster_manual[88:93]
-	#monster manual is a list of html code taken from parts of the website that 
+	#monster manual is a list of html code taken from the main page of the website
 	monster_manual = parse_monster_data(monster_manual)
-	#parse the html code for the list of attributes. Monster manual becomes a dictionary of 
-	#print monster_manual
+	#parse the html code for the list of attributes. Monster manual becomes a dictionary of monster names, sizes, etc.
 	monster_directory = make_monsters_dataFrame(monster_manual)
 	#monster_directory is a data frame
-	
-	#print("Printing Monster Directory")
-	#print( monster_directory)
 	all_monster_details = get_all_monster_details(monster_directory["name"])
-	#for i in all_monster_details: 
-		#print i,'\n'
 	all_monster_details = make_monster_details_dataFrame(all_monster_details)
-	#for key in all_monster_details:
-		#print key+': '+str(all_monster_details[key])
+	print all_monster_details
